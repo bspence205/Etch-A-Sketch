1ac93b320e5e92dadbdae169b6c8d181c53a5351
bspence205 <77554087+bspence205@users.noreply.github.com>
2021-07-31 22:18:56 -0700

    variables added

---
+++ script.js
@@ -0,0 +1,6 @@
+const gridContainer = document.querySelector("");
+const controlsContainer = document.querySelector("");
+let squaresPerSide = 16;
+let cell = [];
+let currentlyActive = false;
+let currentColor = [];
