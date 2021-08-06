0f9ea178323229723c538da43eced2ee7c4dba15
bspence205 <77554087+bspence205@users.noreply.github.com>
2021-08-05 18:32:11 -0700

    New Grid/Clear

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ let squaresPerSide = 16;
 let cell = [];
 let currentlyActive = false;
 let currentColor = [];
+
+gridContainer.addEventListener("click", function () {
+  togglePen();
+});
+
+//Controls:
+
+//Clear:
+const resetButton = document.querySelector("#clear");
+resetButton.addEventListener("click", clear);
+
+//New Grid:
+const newGridButton = document.querySelector("#new-grid");
+newGridButton.addEventListener("click", function () {
+  clear();
+  createGrid(newSize.value);
+});
